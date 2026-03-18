4e197ed66b63363369cbbe7b45077a8066f1a359
Ayman <ayman@example.com>
2026-03-17 23:06:52 -0400

    chore: replace logo and favicon with high-res clear version

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 export default function Logo({ size = 40 }: { size?: number }) {
     return (
         <Image
-            src="/ZakatFlow_logo.png"
+            src="/ZakatFlow_Logo_Clear.png"
             alt="NisabFlow"
             width={size}
             height={size}
